66ce76dfdbf27b7efe3a7839615a22a8262ec960
EnPeignoir <HClode@protonmail.com>
2021-04-19 14:44:31 +0200

    Lot of updates

--- octo-timetable.py
+++ octo-timetable.py
@@ -1,13 +1,16 @@
 ﻿import discord
-import random 
+import random
 from discord.ext import commands
-from datetime import datetime, date
+from discord.ext.commands import MissingPermissions
+from discord.ext.commands import BadArgument
+from datetime import datetime
+from datetime import date
 import yaml
 import asyncio
 
 debug = False # to get more precise information in the log
 
-botVersion = "1"
+botVersion = "1.1.1"
 
 bot = commands.Bot(command_prefix="!")
 
@@ -62,14 +65,28 @@ theClass6 = [monClass6, tueClass6, wedClass6, thuClass6, friClass6, monClass6, m
 
 schoolTimetableArray = [theClass1, theClass2, theClass3, theClass4, theClass5, theClass6]
 
-jour_semaine = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi",
+jour_semaine = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"
                 "dimanche"]  # I define each day of the week
 
 
 @bot.event
 async def on_ready():  # print in the console when bot wake up
     print(f"[INFO] Bot initialized as excepted (botVersion = {botVersion})")
-    await bot.change_presence(activity=discord.Game("combats les arabes"))
+    await bot.change_presence(activity=discord.Game("Affronte les arabes"))
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, discord.ext.commands.errors.CommandNotFound):
+        await ctx.send("Cette commande n'existe pas <:grimacing:831844920330551338>")
+    if isinstance(error, discord.ext.commands.errors.MissingPermissions):
+        await ctx.send(
+            ("Tu n'as pas la permission de faire cette commande, "
+            f"{ctx.author.mention}"))
+    if isinstance(error, discord.ext.commands.errors.BadArgument):
+        await ctx.send('Tu sais pas écrire une commande, haha!')
+    else:
+        print('Une erreur inconnue est survenue')
+
 
 def pourcentage(mode):
     nowPreciseDate = datetime.now()  # get exact date with seconds
@@ -84,14 +101,15 @@ def pourcentage(mode):
         else:
             return 100
     elif mode == 1:
-        return round((((int(nowPreciseDate.strftime("%H"))) * 60) + \
+        return round((((int(nowPreciseDate.strftime("%H"))) * 60) +
             (int(nowPreciseDate.strftime("%M")))) / 1440 * 100, 1)
 
-def sendHoraire(ctx, minute, hour, currentDay):
+
+def sendHoraire(ctx, minute, hour, currentDay): # TODO REWRITE sendHoraire
     schoolRole = 0  # need to be referenced before assignement
     classesFromConfig = yaml.load(open("config.yaml"), Loader=yaml.FullLoader).get("classes")
-    roleClass1, roleClass2, roleClass3, roleClass4, roleClass5, roleClass6 = "","","","","",""
-    schoolRoleArray = [roleClass1, roleClass2, roleClass3, roleClass4, roleClass5, roleClass6]
+    role_class1, role_class2, role_class3, role_class4, role_class5, role_class6 = "","","","","",""
+    schoolRoleArray = [role_class1, role_class2, role_class3, role_class4, role_class5, role_class6]
     hourList = yaml.load(open("config.yaml"), Loader=yaml.FullLoader).get("hours")
     minuteList = yaml.load(open("config.yaml"), Loader=yaml.FullLoader).get("minutes")
     for y in range(len(classesFromConfig)):
@@ -112,8 +130,8 @@ def sendHoraire(ctx, minute, hour, currentDay):
     else:
         incHour = 0  # incrementation of this value in the while to use it properly
         while period == -1:
-            if hour == hourList[incHour] and minute > minuteList[incHour] or hour == hourList[incHour] and minute <= \
-                    minuteList[incHour]:
+            if hour == hourList[incHour] and minute > minuteList[incHour] or hour == \
+            hourList[incHour] and minute <= minuteList[incHour]:
                 period = incHour
             else:
                 incHour += 1
@@ -130,12 +148,16 @@ def sendHoraire(ctx, minute, hour, currentDay):
             descriptionEmbed = ""
             for heurePeriod in range(8 - period):  # because i only want the bot to print today's remaining courses
                 theLeftOfUs += 1
-                descriptionEmbed += (f"\n {heurePeriod + period + 1}h {schoolTimetableArray[schoolRole][currentDay][period + theLeftOfUs]}")
-            descriptionEmbed += (f"\n\n{pourcentage(0)}% de la journée scolaire est écoulé et {pourcentage(1)}% de la journée est écoulé")
-            embed=discord.Embed(title=titleEmbed, description=descriptionEmbed, color=0x03c2fc)
-            embed.set_author(name=f"{ctx.message.author.name} ({classesFromConfig[schoolRole]})", icon_url=ctx.message.author.avatar_url)
+                descriptionEmbed += (
+                    f"\n {heurePeriod + period + 1}h {schoolTimetableArray[schoolRole][currentDay][period + theLeftOfUs]}")
+            descriptionEmbed += (
+                f"\n\n{pourcentage(0)}% de la journée scolaire est écoulé et {pourcentage(1)}% de la journée est écoulé")
+            embed=discord.Embed(title=titleEmbed, description=descriptionEmbed, color=0xf48224)
+            embed.set_author(name=
+                f"{ctx.message.author.display_name} ({classesFromConfig[schoolRole]})", icon_url=ctx.message.author.avatar_url)
             embed.set_footer(text="Powered by KIYU Industries")
             return embed
+
 
 @bot.command(name="horaire")
 async def timetable(ctx, jour=None):
@@ -143,27 +165,33 @@ async def timetable(ctx, jour=None):
         dayWeek = date.today()  # get day as a string
         currentDay = date.weekday(dayWeek)  # get day in integrer 0=monday,1=tuesday,...
         nowPreciseDate = datetime.now()  # get exact date with seconds
-        await ctx.send(embed=sendHoraire(ctx, int(nowPreciseDate.strftime("%M")), int(nowPreciseDate.strftime("%H")), currentDay))
+        await ctx.send(
+            embed=sendHoraire(ctx, int(nowPreciseDate.strftime("%M")), int(nowPreciseDate.strftime("%H")), currentDay))
         if debug == True:
             print(f"[DEBUG] !horaire : Today timetable printed in channel ID {ctx.channel.id}")
     else:
         for x in range(len(jour_semaine)):
             if jour == jour_semaine[x]:
                 if x == 5 or x == 6:
-                    await ctx.send("Où est-ce que tu vois qu'on à école ? **T'es con ou quoi ?**",
-                                   file=discord.File("Assets\images\myschoolweek_fr.jpg"))
-                    print(f"[DEBUG] !horaire : {jour_semaine[x]} joke printed in channel ID {ctx.channel.id}")
+                    await ctx.send(
+                        "Où est-ce que tu vois qu'on à école ? **T'es con ou quoi ?**",
+                        file=discord.File("Assets\images\myschoolweek_fr.jpg"))
+                    print(
+                        (f"[DEBUG] !horaire : {jour_semaine[x]} joke"
+                        f"printed in channel ID {ctx.channel.id}"))
                 else:
-                    embedToPrint = sendHoraire(ctx, 0, 6, x)
-                    await ctx.send(embed=embedToPrint)
-                    print(f"[DEBUG] !horaire : {jour_semaine[x]} timetable printed in channel ID {ctx.channel.id}")
+                    await ctx.send(embed=sendHoraire(ctx, 0, 6, x))
+                    print(
+                        (f"[DEBUG] !horaire : {jour_semaine[x]} timetable"
+                         f"printed in channel ID {ctx.channel.id}"))
     await asyncio.sleep(5)
     await ctx.message.delete()
+
 
 @bot.command(name="mails")
 async def mails(ctx):
     RandomMail = random.randrange(1, 12)
-    await ctx.send(file=discord.File(f"Assets\images\MailsDB\MailZeippen{RandomMail}.png"))
+    await ctx.send(file=discord.File(f"Assets/images/MailsDB/MailZeippen{RandomMail}.png"))
     if debug:
         print(f"[DEBUG] !mails : Image {RandomMail} printed in channel ID {ctx.channel.id}")
     await asyncio.sleep(5)
@@ -181,81 +209,176 @@ async def bamboula(ctx):
 async def love(ctx, Personne1, Personne2):
     if ctx.message.channel.id == 763352957579690018:
         botChannel = discord.utils.get(ctx.guild.channels, id=768194273970880533)
-        messageBot = await ctx.send(f"Va faire cette commande dans {botChannel.mention} ou je te soulève <:rage:831149184895811614>") 
+        messageBot = await ctx.send(
+            (f"Va faire cette commande dans {botChannel.mention}"
+            "ou je te soulève <:rage:831149184895811614>"))
         await asyncio.sleep(5)
         await ctx.message.delete()
         await messageBot.delete()
         return
-    wordBanList = {'@everyone', '@here', '<@&763489250162507809>','<@&777564025432965121>','<@&822200827347075132>',
-                    '<@&763815680306184242>','<@&764422266560839680<','<@&763815728972300338>','<@&763815728972300338>',
-                    '<@&763815228323528725>','<@&763815784904261632>','<@&764422166116171806>','<@&764422057353936897>',
-                    '<@&804807279043674143>','<@&828664814678179861>','<@&823562218095640646>','<@&823638574809219163>'}
-    LoveRate = random.randrange(0, 100)
+
+    wordBanList = {'@everyone', '@here', '<@&763489250162507809>',
+                   '<@&777564025432965121>','<@&822200827347075132>',
+                   '<@&763815680306184242>','<@&764422266560839680<',
+                   '<@&763815728972300338>','<@&763815728972300338>',
+                   '<@&763815228323528725>','<@&763815784904261632>',
+                   '<@&764422166116171806>','<@&764422057353936897>',
+                   '<@&804807279043674143>','<@&828664814678179861>',
+                   '<@&823562218095640646>','<@&823638574809219163>'}
     if Personne1 in wordBanList or Personne2 in wordBanList:
         await ctx.send(f"Tu t'es pris pour qui ? {ctx.message.author.mention}")
         if debug:
-            print("[DEBUG] !love : Someone tried to use a banned word !")     
-    else:
-        await ctx.send(f"L'amour entre **{Personne1}** et **{Personne2}** est de **{LoveRate}%** <:flushed:830502924479758356>") 
-        if debug:
-            print(f"[DEBUG] !love : The love rate ({LoveRate}%) between {Personne1} and {Personne2} has been printed in channel ID {ctx.channel.id}") 
-        
+            print("[DEBUG] !love : Someone tried to use a banned word !")
+        return
+    
+    LoveRate = random.randrange(0, 100)
+    await ctx.send(
+        (f"L'amour entre **{Personne1}** et **{Personne2}**"
+        f" est de **{LoveRate}%** <:flushed:830502924479758356>"))
+    if debug:
+        print(
+            (f"[DEBUG] !love : The love rate ({LoveRate}%) between {Personne1}"
+            f" and {Personne2} has been printed in channel ID {ctx.channel.id}"))
 
-@bot.command(name="dumont", aliases=["tempmute"])
+
+@bot.command(name="dumont", aliases=["tempmute", "fortnite", "michou"])
 async def dumont(ctx, userArgs: discord.Member = None):
 
-    roleAdmin = discord.utils.get(ctx.guild.roles, name="*")
-    roleMute = discord.utils.get(ctx.guild.roles, name="Mute")
-    channelRetenue = discord.utils.get(ctx.guild.channels, id=769125453314261002)
+    admin_role = discord.utils.get(ctx.guild.roles, name="*")
+    mute_role = discord.utils.get(ctx.guild.roles, name="Mute")
+    afk_channel = discord.utils.get(ctx.guild.channels, id=769125453314261002)
 
     authorizedMembers = {225282512438558720}
     user = ctx.message.author
-    
-    if userArgs != None:
-        if user.id in authorizedMembers or roleAdmin in ctx.author.roles:
-            user = userArgs
-            await ctx.send(f"{user.mention} vient de se faire envoyer en retenue par Mr Dumont pendant 3 minutes! <:angry:830881956870357023>")
-        else:
-            await ctx.send(f"Tu t'es cru où ? {user.mention}, va en retenue! <:angry:830881956870357023>")
-    else:
-        await ctx.send(f"{user.mention} vient de se faire envoyer en retenue par Mr Dumont pendant 3 minutes! <:angry:830881956870357023>")
 
-    await user.add_roles(roleMute)
+    if userArgs != None:
+        if user.id in authorizedMembers or admin_role in ctx.author.roles:
+            user = userArgs
+            await ctx.send(
+                (f"{user.mention} vient de se faire envoyer en retenue par "
+                "Mr Dumont pendant 3 minutes! <:angry:830881956870357023>"))
+        else:
+            await ctx.send(
+                f"Tu t'es cru où ? {user.mention}, va en retenue! <:angry:830881956870357023>")
+    else:
+        await ctx.send(
+            (f"{user.mention} vient de se faire envoyer en retenue par " 
+            "Mr Dumont pendant 3 minutes! <:angry:830881956870357023>"))
+
+    await user.add_roles(mute_role)
     await ctx.message.delete()
     if user.voice != None:
-        await user.move_to(channelRetenue)
+        await user.move_to(afk_channel)
     await asyncio.sleep(180)
-    await user.remove_roles(roleMute)
+    await user.remove_roles(mute_role)
+
 
 @bot.command(name="predict", aliases=["p"])
 async def predict(ctx, args):
     if ctx.message.channel.id == 763352957579690018:
-        botChannel = discord.utils.get(ctx.guild.channels, id=768194273970880533)
-        messageBot = await ctx.send(f"Va faire cette commande dans {botChannel.mention} ou je te soulève <:rage:831149184895811614>")
+        bot_channel = discord.utils.get(ctx.guild.channels, id=768194273970880533)
+        bot_message = await ctx.send(
+            (f"Va faire cette commande dans {bot_channel.mention} "
+            "ou je te soulève <:rage:831149184895811614>"))
         await asyncio.sleep(5)
         await ctx.message.delete()
-        await messageBot.delete()
+        await bot_message.delete()
         return
     predictE = "Prédiction <:crystal_ball:830965316468867082> : "
 
-    predictPhrases = ["Oui","Non", "Peut-Être <:thinking:830966162824888320>",
-                          "Je ne devrais peut-être pas le dire <:face_with_hand_over_mouth:831149509287084052>",
-                          "Sans aucun doute", "C'est probable", "Je ne pense pas", "Probablement pas",
-                          "Je ne suis pas sûr <:face_with_raised_eyebrow:831150009436995584>",
-                          "Il semblerait que oui", "Il semblerait que non...","La réponse pourrait vous choquer... "]
+    predictPhrases = [
+        "Oui","Non", "Peut-Être <:thinking:830966162824888320>",
+        ("Je ne devrais peut-être pas le dire" 
+        "<:face_with_hand_over_mouth:831149509287084052>"), "Sans aucun doute",
+         "C'est probable", "Je ne pense pas", "Probablement pas",
+        "Je ne suis pas sûr <:face_with_raised_eyebrow:831150009436995584>",
+        "Il semblerait que oui", "Il semblerait que non...",
+        "La réponse pourrait vous choquer... "]
     await ctx.send(predictE + "**"+random.choice(predictPhrases)+"**")
 
-@bot.command(name="insultes", aliases=["orraire", "horraire", "orairre", "horairre", "horzire", "orair", "oraire", "horair", "haraire", "horarie",
-                                       "hroaire", "hraire", "hauraire", "haurair", "haurer"])
+@bot.command(name="join")
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    await channel.connect()
+
+@bot.command(name="leave")
+@commands.has_guild_permissions(ban_members=True)
+async def leave(ctx):
+    try:
+        await ctx.voice_client.disconnect()
+    except:
+        await ctx.send(
+            ("Tu n'as pas la permission d'exécuter cette commande,",
+            f"{ctx.author.mention}"))
+
+@bot.command(name='spam')
+#@commands.has_guild_permissions(manage_messages=True)
+async def spam(ctx, user: discord.User, num, *, message=None):
+    if ctx.message.channel.id == 763352957579690018:
+        bot_channel = discord.utils.get(ctx.guild.channels, id=768194273970880533)
+        bot_message = await ctx.send(
+            (f"Va faire cette commande dans {bot_channel.mention} "
+            "ou je te soulève <:rage:831149184895811614>"))
+        await asyncio.sleep(5)
+        await ctx.message.delete()
+        await bot_message.delete()
+        return
+    wordBanList = {'@everyone', '@here', '<@&763489250162507809>',
+                   '<@&777564025432965121>','<@&822200827347075132>',
+                   '<@&763815680306184242>','<@&764422266560839680<',
+                   '<@&763815728972300338>','<@&763815728972300338>',
+                   '<@&763815228323528725>','<@&763815784904261632>',
+                   '<@&764422166116171806>','<@&764422057353936897>',
+                   '<@&804807279043674143>','<@&828664814678179861>',
+                   '<@&823562218095640646>','<@&823638574809219163>'}
+    num = int(num)
+    if num > 10:
+        num = 10
+        ctx.send("*J'ai réajusté la limite à 10, gros spammer*")
+    if user in wordBanList:
+        for x in range(num):
+            await ctx.send(f'{ctx.message.mention.mention}')
+        return
+    if message == None:
+        for x in range(num):
+            await ctx.send(f'{user.mention}')
+        return
+    for x in range(num):
+        await ctx.send(f'{user.mention} {message}')
+    await ctx.message.delete()
+
+        
+
+
+
+@bot.command(name="insultes",
+    aliases=["orraire", "horraire", "orairre", "horairre", "horzire", "orair",
+             "oraire", "horair", "haraire", "horarie", "hroaire", "hraire", 
+             "hauraire", "haurair", "haurer"])
 async def insultes(ctx):
-    insultes = ["Apprend à écrire gros con", "**issou**", "Tu écris comme Prem", "Y'a de l'autisme dans l'air la non ?",
-                "quelle orthographe dis donc", "Pire que Manon Boegen", "ew",
-                "https://www.jaitoutcompris.com/rubriques/dictionnaire.php",
-                "https://www.amazon.fr/Dictionnaire-Robert-Junior-illustré-CE-CM-6e/dp/2321015160/ref=sr_1_1",
-                "Tu sais combien de temps ça ma pris pour codé tout ça ? moins que ça t'aurait pris pour corriger ce message",
-                "Je te chie dessus depuis l'espace", "kinda cringe bro", "Mec tu me fais quoi là", "Apprend","Hérétique!!!",
-                "J'arrive te niquer prépare ton cul", "Tu sais même pas écrire une commande", "J'ai même plus envie de répondre",
-                "Pourquoi t'es encore sur ce discord, on t'a toujours ban ?"]
+    insultes = [
+        "Apprend à écrire gros con", "**issou**", "Tu écris comme Prem",
+        "Y'a de l'autisme dans l'air la non ?", "quelle orthographe dis donc",
+        "Pire que Manon Boegen", "ew", 
+        "https://www.jaitoutcompris.com/rubriques/dictionnaire.php",
+        "https://www.amazon.fr/Dictionnaire-Robert-Junior-illustré-CE-CM-6e/dp/2321015160/ref=sr_1_1",
+        ("Tu sais combien de temps ça ma pris pour coder tout ça ?"
+         " moins que ça t'aurait pris pour corriger ce message"),
+        "Je te chie dessus depuis l'espace", "kinda cringe bro", 
+        "Mec tu me fais quoi là", "Apprend", "Hérétique!!!",
+        "J'arrive te niquer prépare ton cul", 
+        "Tu sais même pas écrire une commande", 
+        "J'ai même plus envie de répondre",
+        "Pourquoi t'es encore sur ce discord, on t'a toujours ban ?",
+        "**soupir**","CHALLENGE:JE FERME MA GUEULE PENDANT 24HEURES!!! (ça tourne mal!!)"]
     await ctx.send(random.choice(insultes))
 
-bot.run("NzY0NDk0ODAyMjQyNzY0ODQw.X4HFRA.d8jey-2qHF-g5NE884mCn5eLLRk") # TODO TOKEN
+@bot.command(name="github", aliases=["git"])
+async def git(ctx):
+    await ctx.send("https://github.com/Firminou/octo-timetable")
+    if debug:
+        print(f"[DEBUG] !github : repo sent")
+    await asyncio.sleep(3)
+    await ctx.message.delete()
+
+bot.run("")

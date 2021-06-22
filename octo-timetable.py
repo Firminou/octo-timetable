ec72ae9eda012e40d2d0e3fe5e13725faa44a063
Firminou <38604778+Firminou@users.noreply.github.com>
2021-06-22 23:15:54 +0200

    No more school update
    Removed the !horaire as it is no longer needed

--- octo-timetable.py
+++ octo-timetable.py
@@ -7,64 +7,8 @@ import yaml
 import asyncio
 
 debug = False # to get more precise information in the log
-
-botVersion = "1.2.0"
-
-bot = commands.Bot(command_prefix=["!", "kiyu "])
-
-monClass1 = ["Géographie", "Math", "Sciences", "Anglais", "Français", "Religion", "Anglais", "Math"]
-tueClass1 = ["Histoire", "Religion", "Option (je sais pas mec)", "Math", "Math", "Langue 2", "Informatique/Étude",
-             "Informatique/Étude"]
-wedClass1 = ["Math", "Français", "Français", "Option (je sais pas mec)", "Langue 1", "/", "/", "/"]
-thuClass1 = ["Histoire", "Sciences", "Français", "Langue 2", "Sport", "Langue 1", "Informatique/Étude",
-             "Informatique/Étude"]
-friClass1 = ["Étude", "Géographie", "Sciences", "Français", "Langue 1", "Sport", "Allemand/Étude", "Allemand/Étude"]
-theClass1 = [monClass1, tueClass1, wedClass1, thuClass1, friClass1, monClass1, monClass1]
-
-monClass2 = ["Latin/Étude", "Sport intensif", "Sport intensif", "Anglais", "Sciences", "Latin/Étude", "Religion",
-             "Géographie/Anglais"]
-tueClass2 = ["Math", "Histoire/Anglais", "Math", "Sciences", "Anglais/Histoire", "Allemand/Étude", "Français", "Français"]
-wedClass2 = ["Sciences", "Math", "Anglais/Histoire", "Latin/Étude", "Anglais/Géographie", "/", "/", "/"]
-thuClass2 = ["Math", "Histoire/Anglais", "Géographie/Anglais", "Allemand/Étude", "Français", "Français", "Math", "Étude"]
-friClass2 = ["Labo", "Labo", "Français", "Anglais", "Latin/Étude", "Religion", "Allemand/Étude", "Allemand/Étude"]
-theClass2 = [monClass2, tueClass2, wedClass2, thuClass2, friClass2, monClass2, monClass2]
-
-monClass3 = ["Latin/Étude", "Math", "Math", "Géographie", "Étude", "Latin/Étude", "Sciences", "Anglais"]
-tueClass3 = ["Math", "Anglais", "Français", "Religion", "Histoire", "Allemand/Étude", "Sport/Info", "Sport/Info"]
-wedClass3 = ["Religion", "Français", "Histoire", "Latin/Étude", "Géographie", "/", "/", "/"]
-thuClass3 = ["Math", "Anglais", "Anglais", "Allemand/Étude", "Faire des squats", "Français", "Science", "Français"]
-friClass3 = ["Sciences", "Français", "Math", "Anglais", "Latin/Étude", "Faire des pompes", "Allemand/Étude",
-             "Allemand/Étude"]
-theClass3 = [monClass3, tueClass3, wedClass3, thuClass3, friClass3, monClass3, monClass3]
-
-monClass4 = ["Math", "**Faire la saucisse**", "**Faire des roulades à la zelda", "Géographie", "Religion", "Sciences",
-             "Sciences", "Anglais"]
-tueClass4 = ["Français", "Anglais", "Sciences", "Français", "Histoire", "Allemand/Étude", "Étude", "Étude"]
-wedClass4 = ["Français", "Math", "Histoire", "Sciences", "Géographie", "/", "/", "/"]
-thuClass4 = ["Étude", "Anglais", "Anglais", "Allemand/Étude", "Religion", "Math", "Informatique/Étude",
-             "Informatique/Étude"]
-friClass4 = ["Français", "Math", "Math", "Anglais", "Sciences", "Français", "Allemand/Étude", "Allemand/Étude"]
-theClass4 = [monClass4, tueClass4, wedClass4, thuClass4, friClass4, monClass4, monClass4]
-
-monClass5 = ["Religion", "Histoire", "Français", "Français", "Math", "Sciences", "Sciences", "Étude"]
-tueClass5 = ["Histoire", "Anglais", "Sciences", "**Faire des abdos**", "**Muscler son boule**", "Allemand/Étude", "Étude",
-             "Étude"]
-wedClass5 = ["Anglais", "Math", "Math", "Sciences", "Français", "/", "/", "/"]
-thuClass5 = ["Géographie", "Français", "Anglais", "Allemand/Étude", "Religion", "Math", "Informatique", "Informatique"]
-friClass5 = ["Anglais", "Anglais", "Math", "Géographie", "Sciences", "Français", "Allemand/Étude", "Allemand/Étude"]
-theClass5 = [monClass5, tueClass5, wedClass5, thuClass5, friClass5, monClass5, monClass5]
-
-monClass6 = ["Étude", "Comms", "Comms", "**Sport**", "Soulever des ~~bites~~ poids", "Anglais", "Math", "Math"]
-tueClass6 = ["Science", "AHV", "Rel", "\"**Sport**\"", "\"**Sport**\"", "Sciences", "Histoire-Géo", "Flute"]
-wedClass6 = ["IVP", "EVS", "EVS", "Anglais", "Étude", "/", "/", "/"]
-thuClass6 = ["AHV", "Français", "Français", "**Kazoo**", "Religion", "Math", "Anglais", "Histoire-Géo"]
-friClass6 = ["AHV", "IVP", "IVP", "Math", "Français", "Français", "Anglais", "Étude"]
-theClass6 = [monClass6, tueClass6, wedClass6, thuClass6, friClass6, monClass6, monClass6]
-
-schoolTimetableArray = [theClass1, theClass2, theClass3, theClass4, theClass5, theClass6]
-
-jour_semaine = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"
-                "dimanche"]  # I define each day of the week
+botVersion = '1.2.1'
+bot = commands.Bot(command_prefix=["!", "kiyu ", 'gros con'])
 
 
 @bot.event
@@ -87,103 +31,10 @@ async def on_command_error(ctx, error):
         print('Une erreur inconnue est survenue')
 
 
-def pourcentage(mode):
-    nowPreciseDate = datetime.now()  # get exact date with seconds
-    toPourcent = (int(nowPreciseDate.strftime("%H"))) * 60 + \
-        int(nowPreciseDate.strftime("%M"))
-    if mode == 0:
-        if toPourcent <= 480:
-            return 0
-        elif 480 > toPourcent or toPourcent < 955:
-            toPourcent = (toPourcent - 480) / 475 * 100
-            return round(toPourcent, 1)
-        else:
-            return 100
-    elif mode == 1:
-        return round((((int(nowPreciseDate.strftime("%H"))) * 60) +
-            (int(nowPreciseDate.strftime("%M")))) / 1440 * 100, 1)
-
-
-def sendHoraire(ctx, minute, hour, currentDay): # TODO REWRITE sendHoraire
-    schoolRole = 0  # need to be referenced before assignement
-    classesFromConfig = yaml.load(open("config.yaml"), Loader=yaml.FullLoader).get("classes")
-    role_class1, role_class2, role_class3, role_class4, role_class5, role_class6 = "","","","","",""
-    schoolRoleArray = [role_class1, role_class2, role_class3, role_class4, role_class5, role_class6]
-    hourList = yaml.load(open("config.yaml"), Loader=yaml.FullLoader).get("hours")
-    minuteList = yaml.load(open("config.yaml"), Loader=yaml.FullLoader).get("minutes")
-    for y in range(len(classesFromConfig)):
-        schoolRoleArray[y] = discord.utils.get(ctx.guild.roles, name=classesFromConfig[y])
-    for ima in range(len(classesFromConfig)):
-        if schoolRoleArray[ima] in ctx.author.roles:
-            schoolRole = ima
-
-    period = -1
-    if currentDay > 4:
-        period = 0
-        currentDay = 0
-    elif hour >= hourList[9]+1 or hour == hourList[9] and minute > minuteList[9]:
-        period = 0
-        currentDay += 1
-    elif hour < hourList[0]:
-        period = 0
-    else:
-        incHour = 0  # incrementation of this value in the while to use it properly
-        while period == -1:
-            if hour == hourList[incHour] and minute > minuteList[incHour] or hour == \
-            hourList[incHour] and minute <= minuteList[incHour]:
-                period = incHour
-            else:
-                incHour += 1
-        if period != 0:
-            period -= 1
-    if debug == True:
-        print(f"[DEBUG] Day: {currentDay} Period: {period} Hour: {hour} Minute: {minute} ClassRole: {schoolRole}")
-
-    for jour in range(7):  # because there are 7 day in a week
-        if currentDay == jour:  # this way i use the day i got as an useful variable
-            theLeftOfUs: int = -1  # value to increment to give us the remaining courses
-            titleEmbed = f"Horaire {jour_semaine[currentDay].capitalize()}\n"
-            descriptionEmbed = ""
-            for heurePeriod in range(8 - period):  # because i only want the bot to print today's remaining courses
-                theLeftOfUs += 1
-                descriptionEmbed += (
-                    f"\n {heurePeriod + period + 1}h {schoolTimetableArray[schoolRole][currentDay][period + theLeftOfUs]}")
-            descriptionEmbed += (
-                f"\n\n{pourcentage(0)}% de la journée scolaire est écoulé et {pourcentage(1)}% de la journée est écoulé")
-            embed=discord.Embed(title=titleEmbed, description=descriptionEmbed, color=0xf48224)
-            embed.set_author(name=
-                f"{ctx.message.author.display_name} ({classesFromConfig[schoolRole]})", icon_url=ctx.message.author.avatar_url)
-            embed.set_footer(text="Powered by KIYU Industries")
-            return embed
-
 
 @bot.command(name="horaire")
-async def timetable(ctx, jour=None):
-    if jour == None:
-        dayWeek = date.today()  # get day as a string
-        currentDay = date.weekday(dayWeek)  # get day in integrer 0=monday,1=tuesday,...
-        nowPreciseDate = datetime.now()  # get exact date with seconds
-        await ctx.send(
-            embed=sendHoraire(ctx, int(nowPreciseDate.strftime("%M")), int(nowPreciseDate.strftime("%H")), currentDay))
-        if debug:
-            print(f"[DEBUG] !horaire : Today timetable printed in channel ID {ctx.channel.id}")
-    else:
-        for x in range(len(jour_semaine)):
-            if jour == jour_semaine[x]:
-                if x == 5 or x == 6:
-                    await ctx.send(
-                        "Où est-ce que tu vois qu'on à école ? **T'es con ou quoi ?**",
-                        file=discord.File("Assets\images\myschoolweek_fr.jpg"))
-                    print(
-                        (f"[DEBUG] !horaire : {jour_semaine[x]} joke"
-                        f"printed in channel ID {ctx.channel.id}"))
-                else:
-                    await ctx.send(embed=sendHoraire(ctx, 0, 6, x))
-                    print(
-                        (f"[DEBUG] !horaire : {jour_semaine[x]} timetable"
-                         f"printed in channel ID {ctx.channel.id}"))
-    await asyncio.sleep(5)
-    await ctx.message.delete()
+async def timetable(ctx):
+    await ctx.channel.send('ON à pas école gros con')
 
 
 @bot.command(name="mails")
@@ -230,7 +81,7 @@ async def love(ctx, Personne1, Personne2):
         if debug:
             print("[DEBUG] !love : Someone tried to use a banned word !")
         return
-    
+
     LoveRate = random.randrange(0, 100)
     await ctx.send(
         (f"L'amour entre **{Personne1}** et **{Personne2}**"
@@ -248,7 +99,7 @@ async def dumont(ctx, userArgs: discord.Member = None):
     mute_role = discord.utils.get(ctx.guild.roles, name="Mute")
     afk_channel = discord.utils.get(ctx.guild.channels, id=769125453314261002)
 
-    authorizedMembers = {225282512438558720}
+    authorizedMembers = {225282512438558720,357931867443560448,669992459060772864}
     user = ctx.message.author
 
     if userArgs != None:
@@ -262,7 +113,7 @@ async def dumont(ctx, userArgs: discord.Member = None):
                 f"Tu t'es cru où ? {user.mention}, va en retenue! <:angry:830881956870357023>")
     else:
         await ctx.send(
-            (f"{user.mention} vient de se faire envoyer en retenue par " 
+            (f"{user.mention} vient de se faire envoyer en retenue par "
             "Mr Dumont pendant 3 minutes! <:angry:830881956870357023>"))
 
     await user.add_roles(mute_role)
@@ -288,12 +139,13 @@ async def predict(ctx, args):
 
     predictPhrases = [
         "Oui","Non", "Peut-Être <:thinking:830966162824888320>",
-        ("Je ne devrais peut-être pas le dire" 
+        ("Je ne devrais peut-être pas le dire"
         "<:face_with_hand_over_mouth:831149509287084052>"), "Sans aucun doute",
-         "C'est probable", "Je ne pense pas", "Probablement pas",
+        "C'est probable", "Je ne pense pas", "Probablement pas",
         "Je ne suis pas sûr <:face_with_raised_eyebrow:831150009436995584>",
-        "Il semblerait que oui", "Il semblerait que non...",
-        "La réponse pourrait vous choquer... "]
+        "Il semblerait que oui", "Il semblerait que non...", 'Impossible',
+        "La réponse pourrait vous choquer... ", 'ça c\'est sur à 100%', 'Faut pas rêver', 'C\'est ton destin',
+        'Peu de chance', 'Et c\'est un non !', 'N\'y compte pas', 'Autant de chance qu\'une Isma sans échecs :)']
     await ctx.send(predictE + "**"+random.choice(predictPhrases)+"**")
 
 
@@ -301,6 +153,7 @@ async def predict(ctx, args):
 async def join(ctx):
     channel = ctx.author.voice.channel
     await channel.connect()
+
 
 @bot.command(name="leave")
 @commands.has_guild_permissions(ban_members=True)
@@ -352,24 +205,25 @@ async def spam(ctx, user: discord.User, num, *, message=None):
 
 @bot.command(name="insultes",
     aliases=["orraire", "horraire", "orairre", "horairre", "horzire", "orair",
-             "oraire", "horair", "haraire", "horarie", "hroaire", "hraire", 
+             "oraire", "horair", "haraire", "horarie", "hroaire", "hraire",
              "hauraire", "haurair", "haurer"])
 async def insultes(ctx):
     insultes = [
         "Apprend à écrire gros con", "**issou**", "Tu écris comme Prem",
         "Y'a de l'autisme dans l'air la non ?", "quelle orthographe dis donc",
-        "Pire que Manon Boegen", "ew", 
+        "Pire que Manon Boegen", "ew",
         "https://www.jaitoutcompris.com/rubriques/dictionnaire.php",
         "https://www.amazon.fr/Dictionnaire-Robert-Junior-illustré-CE-CM-6e/dp/2321015160/ref=sr_1_1",
-        ("Tu sais combien de temps ça ma pris pour coder tout ça ?"
-         " moins que ça t'aurait pris pour corriger ce message"),
-        "Je te chie dessus depuis l'espace", "kinda cringe bro", 
+        "Tu sais combien de temps ça ma pris pour coder tout ça ?"
+        "moins que ça t'aurait pris pour corriger ce message",
+        "Je te chie dessus depuis l'espace", "kinda cringe bro",
         "Mec tu me fais quoi là", "Apprend", "Hérétique!!!",
-        "J'arrive te niquer prépare ton cul", 
-        "Tu sais même pas écrire une commande", 
+        "J'arrive te niquer prépare ton cul",
+        "Tu sais même pas écrire une commande",
         "J'ai même plus envie de répondre",
         "Pourquoi t'es encore sur ce discord, on t'a toujours ban ?",
-        "**soupir**","CHALLENGE:JE FERME MA GUEULE PENDANT 24HEURES!!! (ça tourne mal!!)"]
+        "**soupir**","CHALLENGE:JE FERME MA GUEULE PENDANT 24HEURES!!! (ça tourne mal!!)",
+        'Tu es le fils de Zeippen ou quoi ?', 'J\'appele l\'orphelinat']
     await ctx.send(random.choice(insultes))
 
 
@@ -389,8 +243,6 @@ async def reponse(ctx, *,text):
 
 @bot.command(name="sarcasme",aliases=["majuscule", "maj", "sarcasm"])
 async def sarcasm(ctx, *,sentence):
-    #/!\ I DID NOT WRITE THIS CODE HERE IS THE GITHUB REPO FORM WHERE IT COME FROM
-    #https://github.com/peterlravn/My-projects
     new_sentence = ""
     number = 0  # Dummy number for tracking
 
@@ -420,4 +272,4 @@ async def sarcasm(ctx, *,sentence):
     await ctx.send(new_sentence)
     await ctx.message.delete()
 
-bot.run("")
+bot.run('')
